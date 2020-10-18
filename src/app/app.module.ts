b56e57033081d770bf84b29ee7b0d2d8f9a9d524
Neibeth Chango <neiichango@gmail.com>
2020-10-18 01:01:40 -0600

    header y footer listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    // importar otras dependencias que sean necesario cargar en el componente principal.
+// importar los módulos creados propios en orden
+    CoreModule,
+    ShareModule,
+
+    // después los demás módulos
+
+    HomeModule,
+    UserModule,
+// al final el gestor de las rutas principal
+
+     AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
